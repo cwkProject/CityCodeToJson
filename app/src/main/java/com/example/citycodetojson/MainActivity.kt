7f23ef762928f202b9ab45736c2416bb31fd9365
超悟空 <39251474@163.com>
2018-08-23 10:30:42 +0800

    使用高德数据源，增加自治市支持，增加香港澳门支持

--- app/src/main/java/com/example/citycodetojson/MainActivity.kt
+++ app/src/main/java/com/example/citycodetojson/MainActivity.kt
@@ -2,12 +2,12 @@ package com.example.citycodetojson
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Toast
 import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
-import java.util.*
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
@@ -36,21 +36,24 @@ class MainActivity : AppCompatActivity() {
             val cityMap = cityList.groupBy {
                 when {
                     it.id.endsWith("0000") -> "province"
+                    it.id.startsWith("81") || it.id.startsWith("82") -> "city" // 香港和澳门
                     it.id.endsWith("00") -> "city"
                     else -> "district"
                 }
             }
 
-            zip(cityMap["city"]!!, cityMap["district"]!!, 4)
+            val lose = zip(cityMap["city"]!!, cityMap["district"]!!, 4)
 
-            cityMap["city"]!!.forEach {
+            val citys = cityMap["city"]!! + lose
+
+            citys.forEach {
                 if (it.cityList == null) {
                     it.cityList = ArrayList()
                     it.cityList?.add(City(it.id, " "))
                 }
             }
 
-            zip(cityMap["province"]!!, cityMap["city"]!!, 2)
+            zip(cityMap["province"]!!, citys, 2)
 
             val newJson = gson.toJson(cityMap["province"])
 
@@ -66,16 +69,27 @@ class MainActivity : AppCompatActivity() {
 
     /**
      * 合并元素，将child添加到对应parent的cityList字段
+     *
+     * @return 未添加到parent的子元素集合
      */
-    private fun zip(parent: List<City>, child: List<City>, sampling: Int) {
+    private fun zip(parent: List<City>, child: List<City>, sampling: Int): List<City> {
+        val loseList = ArrayList<City>()
+
         child.forEach { c ->
-            parent.find { it.id.startsWith(c.id.take(sampling)) }?.let {
+            val result = parent.find { it.id.startsWith(c.id.take(sampling)) }?.let {
                 if (it.cityList == null) {
                     it.cityList = ArrayList()
                 }
 
                 it.cityList?.add(c)
             }
+
+            if (result == null) {
+                loseList += c
+                Log.v("zip", "lose name:${c.name}")
+            }
         }
+
+        return loseList
     }
 }
